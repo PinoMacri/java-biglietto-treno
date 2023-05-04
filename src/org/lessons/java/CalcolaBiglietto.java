d6426fe6f8132204fc3d1d6167ccd2e42ab6d2c5
PinoMacri <pinonegozio1@outlook.it>
2023-05-04 15:06:49 +0200

    consegnato

--- src/org/lessons/java/CalcolaBiglietto.java
+++ src/org/lessons/java/CalcolaBiglietto.java
@@ -1,5 +1,23 @@
 package org.lessons.java;
 
-public class CalcolaBiglietto {
+import java.util.Scanner;
 
+public class CalcolaBiglietto {
+public static void main(String[]args) {
+	Scanner in = new Scanner (System.in);
+	System.out.println("Quanti KM vuoi percorrere?");
+	int userStr = in.nextInt();
+	System.out.println("Qual'è la tua età?");
+	int userStr2 = in.nextInt();
+	if (userStr2 < 18) {
+		int scontoMinorenni = userStr*20/100;
+		userStr = userStr - scontoMinorenni;
+	}
+	if (userStr2 > 65) {
+		int scontoOver = userStr*40/100;
+		userStr = userStr-scontoOver;
+	}
+	System.out.println(userStr * 0.21);
+	in.close();
+}
 }
